0e72ade3e57f5aaeddfe2d3b5e794db34fcdcea3
wakakaxz <wakakaxiaozheng@163.com>
2022-09-28 18:51:35 +0800

    添加静态页面访问映射

--- src/main/java/com/xz/seckill/config/WebConfig.java
+++ src/main/java/com/xz/seckill/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -23,5 +24,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         // 判断登录解析器
         resolvers.add(userArgumentResolver);
+    }
+
+    /**
+     * 实现了 WebMvcConfigurer, 就需要手动设置静态资源访问处理
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 静态页面访问
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
